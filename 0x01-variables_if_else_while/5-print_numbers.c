cd1abe117e25776ee525e0154153693a2373096e
0498drew <mwaseandrew98@gmail.com>
2022-06-04 08:53:29 -0700

    program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ * Description: prints all single dig * numbers of base  
+ * Return: 0
+ */
+int main(void)
+{
+		int a;
+
+			for (a = 0; a < 10; a++)
+						printf("%i", a);
+				printf("\n");
+					return (0);
+}
+
+
