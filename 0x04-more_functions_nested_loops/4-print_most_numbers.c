3ac92615d65854b09d0cf9cd88d2fa03abfc0669
0498drew <mwaseandrew98@gmail.com>
2022-06-09 18:28:37 -0700

    Write a function that prints the numbers, from 0 to 9, followed by a new line.

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,19 @@
+#include "main.h"
 
+/**
+ * print_most_numbers - print from 0 to 9 without printing 2 and 4
+ *
+ * Return: Nothing
+ */
+void print_most_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		if (n == '2' || n == '4')
+			continue;
+	}
+
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-#include <stdio.h>
+
 /**
  * main - entry point
  *
@@ -16,11 +16,18 @@ int main(void)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 			printf("FizzBuzz");
-		else if (n % 3 == 0)
+		else if (n % 5 == 0)
 			printf("Buzz");
 		else if (n % 3 == 0)
 			printf("Fizz");
 		else
 			printf("%i", n);
+
+		if (n < 100)
+			printf(" ");
+		else
+			printf("\n");
 	}
+	return (0);
+
 }
