4e831be65d6340ad7eff41ab4b3c82f042efddd1
0498drew <mwaseandrew98@gmail.com>
2022-06-13 14:20:40 -0700

    a function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
 
 int is_numerical(unsigned int n)
 {
-	return (n >= '0' &&  n <= '9');
+return (n >= '0' &&  n <= '9');
 }
 
 /**
@@ -19,30 +19,28 @@ int is_numerical(unsigned int n)
 
 int _atoi(char *s)
 {
-	unsigned int number, i;
-	int sign;
+unsigned int number, i;
+int sign;
 
-	sign = 1;
-	number = 0;
+sign = 1;
+number = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (is_numerical(s[i]))
-		{
-			number = (s[i] - 48) + number * 10;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (is_numerical(s[i]))
+{
+number = (s[i] - 48) + number * 10;
 
-			if (s[i + 1] == ' ')
-				break;
-		}
-		else if (s[i] == '-')
-		{
-			sign *= -1;
-		}
-
-	}
-
-	return (number *sign);
-
+if (s[i + 1] == ' ')
+break;
+}
+else if (s[i] == '-')
+{
+sign *= -1;
+}
 
 }
 
+return (number *sign);
+
+}
--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,18 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
+ *main - generates random valid passwords
+ *Return: 0 (on success).
+ *
  */
+
 int main(void)
 {
 	int num;
 
-	srand(time(0));
+	srand(time(NULL));
 	sum = 0;
 	while (sum <= 2645)
 	{
