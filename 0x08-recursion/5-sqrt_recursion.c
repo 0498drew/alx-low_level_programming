e5c47d9c8de5fe0c4b34e5fe9b11fc56532c2080
0498drew <mwaseandrew98@gmail.com>
2022-06-22 09:07:52 -0700

    Write a function that returns the natural square root of a number.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,19 +9,19 @@
 
 int _evaluate(int i, int n)
 {
-	/*Evaluate function*/
-	if (n == 0 || n == 1)
-		return (n);
+/*Evaluate function*/
+if (n == 0 || n == 1)
+return (n);
 
-	else if (i * i < n)
-		return (_evaluate(i + 1, n));
+else if (i * i < n)
+return (_evaluate(i + 1, n));
 
-	else if (i * i == n) /*Condiction base*/
-		return (i);
+else if (i * i == n) /*Condiction base*/
+return (i);
 
-	return (-1);
+return (-1);
 
-	return (-1);
+return (-1);
 }
 
 /**
@@ -32,14 +32,14 @@ int _evaluate(int i, int n)
 
 int _sqrt_recursion(int n)
 {
-	int i = 0;
+int i = 0;
 
-	if (i < 0) /*If n is negative*/
-		return (-1);
+if (i < 0) /*If n is negative*/
+return (-1);
 
-	else
-	{
-		return (_evaluate(i, n)); /*Recursive call*/
-	}
+else
+{
+return (_evaluate(i, n)); /*Recursive call*/
+}
 
 }
