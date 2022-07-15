480a5ec1437dc709aaa83c101ed43c0060e73939
0498drew <mwaseandrew98@gmail.com>
2022-07-14 18:44:58 -0700

    aline

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+
+#include "lists.h"
+#include "stdio.h"
+/**
+ * myFuncode - this function runs before main
+ */
+void myFuncode(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
