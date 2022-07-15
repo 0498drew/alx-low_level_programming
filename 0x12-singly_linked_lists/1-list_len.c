3ac893178bb499d60d67b4122e309b17cfc85d7c
0498drew <mwaseandrew98@gmail.com>
2022-07-14 19:41:34 -0700

    errors

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,34 +1,22 @@
-
-
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - function that prints all the elements of a list_t list
+ * list_len -function that return numbers of elements in a
+ * linked  list
  * @h: header pointer
- * Return: number of nodes
-*/
+ * Return: number of lits's elements
+ */
 
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
-	size_t cont = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-			h = h->next;
-			cont++;
-		}
-		else
-		{
-			printf("[%u] %s\n", h->len, h->str);
-			h = h->next;
-			cont++;
-		}
+		h = h->next;
+		count++;
 	}
-	return (cont);
+	return (count);
 }
+
 
