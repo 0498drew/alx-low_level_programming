d519fb9f2c790c5a74a81c183a2f1ddecfa3ca15
0498drew <mwaseandrew98@gmail.com>
2022-07-20 14:22:02 -0700

    freeze listint_t

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,26 +1,26 @@
 #include "lists.h"
-#include "stdlib.h"
 
 /**
- * free_listint2 - a function free list_t
- * @head: the head of linked list
- * Return:void
+ * free_listint2 - Frees a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ *
+ * Description: Sets the head to NULL.
  */
-
 void free_listint2(listint_t **head)
 {
-listint_t *tmp;
-listint_t *traverse;
+	listint_t *tmp;
 
-if (head == NULL)
-return;
-traverse = (*head);
-while (traverse != NULL)
-}
-tmp = traverse;
-traverse = traverse->next;
-free(tmp);
-}
-*head = NULL;
+	if (head == NULL)
+		return;
 
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+
+	head = NULL;
 }
+
