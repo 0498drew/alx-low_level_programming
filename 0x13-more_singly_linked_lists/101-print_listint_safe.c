d122faab46b7c3bd405e257173d0d3d017949c70
0498drew <mwaseandrew98@gmail.com>
2022-07-19 17:54:33 -0700

    Write a function that prints a listint_t linked list.

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+/**
+ * _check_and_print - Check the list and print
+ * @head: of newlist to check
+ * @prev: node on list to check
+ *
+ * Return: number of nodes
+ */
+
+int _check_and_print(const listint_t *head, listint_safe *prev)
+{
+	listint_safe node, *tmp;
+
+	if (head->next == NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		return (1);
+	}
+
+	node.next = prev;
+	node.addy = head;
+	tmp = node.next;
+	while (tmp != NULL && tmp->addy != head)
+		tmp = tmp->next;
+	if (tmp != NULL)
+	{
+		printf("-> [%p] %d\n", (void *)head, head->n);
+		return (0);
+	}
+
+	printf("[%p] %d\n", (void *)head, head->n);
+
+	return (1 + _check_and_print(head->next, &node));
+}
+
+/**
+ * print_listint_safe - Print a linkint_t with user error
+ * @head: head of the string
+ *
+ * Return: number of nodes in list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	if (head == NULL)
+		return (0);
+
+	return (_check_and_print(head, NULL));
+}
